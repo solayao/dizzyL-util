a726226e49687edb084aa3087f97cada2409da6d
Dizzy L <932164534@QQ.com>
2018-11-09 13:55:00 +0800

    Update DizzyHcCrawler.js

--- es/crawler/DizzyHcCrawler.js
+++ es/crawler/DizzyHcCrawler.js
@@ -22,7 +22,7 @@ class DizzyHcCrawler {
         
         process.on('unhandledRejection', (error, p) => {
             // Will print "unhandledRejection err is not defined"
-            WarningConsole('UnhandledRejection', 'DizzyHcCrawler');
+            WarningConsole('UnhandledRejection', __filename, JSON.stringify({p: error}));
             console.log(p);
         });
     }
