9798224a75bba40a8266652b78ba76e27c155ebc
Dizzy L <932164534@QQ.com>
2019-06-21 16:16:34 +0800

    Create index.js

---
+++ es/promise/index.js
@@ -0,0 +1,15 @@
+/**
+ * @description 优雅的处理 async/await
+ * @author Dizzy L
+ * @param {Function} asyncFunc 
+ * @returns {Array} [err, asyncResult] = true: [null, res] / false: [error, null]
+ */
+function promiseWithErrorCaptured(asyncFunc) {
+  try {
+     let res = await asyncFunc();
+     return [null, res];
+  } catch (e) {
+     return [e, null];
+  }
+}
+exports.promiseWithErrorCaptured = promiseWithErrorCaptured;
